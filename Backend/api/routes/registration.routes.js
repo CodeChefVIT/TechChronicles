c8fddc81c90d68d72b4a8e5e84aeb1d5f5314fde
Jugal Bhatt <jugalbhatt3@gmail.com>
2021-03-10 11:50:46 +0530

    added captcha to register uuser form

--- Backend/api/routes/registration.routes.js
+++ Backend/api/routes/registration.routes.js
@@ -1,11 +1,13 @@
 const express = require("express");
 
+const recaptchaVerification = require('../middlewares/recaptcha.js')
+
 const registrationController = require("../controllers/registration.controller");
 
 const router = express.Router();
 
 // Register a user
-router.post("/addUser", registrationController.add);
+router.post("/addUser", recaptchaVerification,registrationController.add);
 
 // Get all registered users
 router.get('/all', registrationController.getAll);
